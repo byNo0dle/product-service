bb9b560c8fd10968be7660710e9b2a5f5e3735f9
Nick <nicksaurinocruz@gmail.com>
2024-01-22 16:32:07 -0500

    The necessary dependencies were implemented to build the project

---
+++ src/test/java/com/ufostyle/productservice/BankProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufostyle.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
