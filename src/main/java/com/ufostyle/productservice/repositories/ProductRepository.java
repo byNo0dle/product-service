fd0a797d26dd403f80598cafb3c6831fb363790d
Nick <nicksaurinocruz@gmail.com>
2024-01-22 17:14:51 -0500

    ProductRepository was implemented

---
+++ src/main/java/com/ufostyle/productservice/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.ufostyle.productservice.repositories;
+
+import com.ufostyle.productservice.entities.Product;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends ReactiveMongoRepository<Product, Long> {
+}
