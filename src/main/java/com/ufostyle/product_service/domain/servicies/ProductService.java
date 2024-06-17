ed298cb30768a63f5c3161bd6e4acf7f037495fe
Nick Saurino <nicksaurinocruz@gmail.com>
2024-06-17 13:48:47 -0500

    Add the ProductService

---
+++ src/main/java/com/ufostyle/product_service/domain/servicies/ProductService.java
@@ -0,0 +1,36 @@
+package com.ufostyle.product_service.domain.servicies;
+
+import com.ufostyle.product_service.domain.entities.ProductEntity;
+import com.ufostyle.product_service.domain.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    ProductRepository productRepository;
+
+    public ArrayList<ProductEntity> findAllProduct() {
+        return (ArrayList<ProductEntity>) productRepository.findAll();
+    }
+
+    public ProductEntity saveProduct(ProductEntity product) {
+        return productRepository.save(product);
+    }
+
+    public Optional<ProductEntity> findProductById(String id) {
+        return productRepository.findById(id);
+    }
+
+    public ProductEntity updateProduct(ProductEntity product) {
+        return productRepository.save(product);
+    }
+
+    public void deleteProduct(String idProduct) {
+        productRepository.deleteById(idProduct);
+    }
+}
