33b665dcbf37993488d3d7d42aab3b9a62fd09af
Nick Saurino <nicksaurinocruz@gmail.com>
2024-06-18 15:11:20 -0500

    Add the response status in the controller

--- src/main/java/com/ufostyle/product_service/infrastructure/controllers/ProductController.java
+++ src/main/java/com/ufostyle/product_service/infrastructure/controllers/ProductController.java
@@ -3,6 +3,7 @@ package com.ufostyle.product_service.infrastructure.controllers;
 import com.ufostyle.product_service.domain.entities.ProductEntity;
 import com.ufostyle.product_service.domain.servicies.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -16,16 +17,19 @@ public class ProductController {
     ProductService productService;
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public ArrayList<ProductEntity> findAllProduct() {
         return productService.findAllProduct();
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public ProductEntity saveProduct(@RequestBody ProductEntity product) {
         return productService.saveProduct(product);
     }
 
     @GetMapping("/{idProduct}")
+    @ResponseStatus(HttpStatus.OK)
     public Optional<ProductEntity> findProductById(@PathVariable("idProduct") String idProduct) {
         return productService.findProductById(idProduct);
     }
